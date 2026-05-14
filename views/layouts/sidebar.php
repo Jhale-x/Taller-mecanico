23aae853a92aedecc64335f8695dc14c8baf9b23
Jhalex <jhairmaldonadoalejandro2000@gmail.com>
2026-05-14 12:05:07 -0500

    Sistema de taller mecánico completo con dashboard, CRUD y relaciones

---
+++ views/layouts/sidebar.php
@@ -0,0 +1,51 @@
+<div class="sidebar">
+
+    <h3>Taller Mecánico</h3>
+
+    <ul>
+
+        <li>
+            <a href="../dashboard.php">
+                Dashboard
+            </a>
+        </li>
+
+        <li>
+            <a href="../clientes/index.php">
+                Clientes
+            </a>
+        </li>
+
+        <li>
+            <a href="../vehiculos/index.php">
+                Vehículos
+            </a>
+        </li>
+
+        <li>
+            <a href="../mecanicos/index.php">
+                Mecánicos
+            </a>
+        </li>
+
+        <li>
+            <a href="../servicios/index.php">
+                Servicios
+            </a>
+        </li>
+
+        <li>
+            <a href="../ordenes/index.php">
+                Órdenes
+            </a>
+        </li>
+
+        <li>
+            <a href="../pagos/index.php">
+                Pagos
+            </a>
+        </li>
+
+    </ul>
+
+</div>
